3909c0b75d649ab14b1ba7866e12d436184a06f5
Julia Marushchenko <marushchenko.yulia@gmail.com>
2025-04-27 16:27:17 +0200

    Add files via upload
    Committing Main class

---
+++ src/com/java/Main.java
@@ -0,0 +1,19 @@
+// Java program to print JAVA with codes of chars
+package com.java;
+
+// Main class
+public class Main {
+
+    // Method main to run java program
+    public static void main(String[] args) {
+
+        // Creating string with value JAVA
+        String word = "JAVA";
+
+        // Printing the value of word to console
+        System.out.println(word);
+
+        // Printing code of symbols of word
+        System.out.println('J' + 'A' + 'V' + 'A');
+    }
+}
